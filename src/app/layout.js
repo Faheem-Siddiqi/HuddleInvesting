4aefbf17d1965b669d36974eab0d0bc372dc0a4d
Faheem-Siddiqi <faheem_siddiqi@yahoo.com>
2024-12-05 09:25:21 +0500

    fix: undo testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
           showSpinner={false}
           color="#005B2F" />
         <Navbar />
-        {/* {children} */}
+        {children}
         <Footer />
       </body>
     </html>
